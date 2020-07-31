7b6622c492925707a97da43d99f80455b29e47a6
Paulo Pires <prpires66@gmail.com>
2020-07-31 00:49:26 -0300

    Commit inicial curso ImersãoReact - Aula 1 - parcial

---
+++ src/components/Menu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png'
+
+function Menu() {
+    return (
+        <header>
+            <img src={Logo} alt='Aluraflix logo' />
+        </header>
+    );
+}
+
+export default Menu;
