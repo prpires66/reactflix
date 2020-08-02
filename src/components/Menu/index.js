4ef01512b1a7cd74107c06067de5a7fa3fceb192
Paulo Pires <prpires66@gmail.com>
2020-08-01 23:15:26 -0300

    Aula 1 - Final

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png'
+import "./Menu.css"
+import Button from '../Button';
 
 function Menu() {
     return (
-        <header>
-            <img src={Logo} alt='Aluraflix logo' />
-        </header>
+        <nav className="Menu">
+            <a href="/">
+               <img className="Logo" src={Logo} alt='Aluraflix logo' />
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo Vídeo
+            </Button>
+
+        </nav>
     );
 }
 
