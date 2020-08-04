26374002562586011c21406ec1f1cfa6dfd12902
Paulo Pires <prpires66@gmail.com>
2020-08-04 01:09:21 -0300

    Aula 2 - Final

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 function ButtonLink(props) {
     // props => { className: "O que alguém passar", href: "/"}
-console.log(props);
     return (
     <a href={props.href} className={props.className}>
         {props.children}
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png'
-import "./Menu.css"
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import "./Menu.css";
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                <img className="Logo" src={Logo} alt='Aluraflix logo' />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
-                Novo Vídeo
+            <Button as={Link} className="ButtonLink" to="cadastro/video">
+                Novo Video
             </Button>
 
         </nav>
