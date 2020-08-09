46e828e2c21cf66678fb1ed21ad5b9c1ed99aadc
Paulo Pires <prpires66@gmail.com>
2020-08-09 03:06:30 -0300

    Aula 4 - Heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -27,6 +27,17 @@ function CadastroCategoria() {
       infosDoEvento.target.value,
     );
   }
+
+  useEffect(() => {
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP)
+    .then(async (respostaDoServidor) => {
+      const resposta = await respostaDoServidor.json();   
+      setCategorias([
+        ...resposta,
+      ]);  
+    });
+  },[]);
 
   return (
     <PageDefault>
@@ -74,6 +85,12 @@ function CadastroCategoria() {
           Cadastrar
         </Button>
       </form>
+
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
 
       <ul>
         {categorias.map((categoria, indice) => (
